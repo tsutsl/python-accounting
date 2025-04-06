56fe0b8d32d2f22db59ce3588e150118c2fa2bc7
tsutsl <76780726+tsutsl@users.noreply.github.com>
2025-04-06 18:06:25 +0400

    Local Session
    Moving from global session listening to the local session listening

--- python_accounting/database/event_listeners.py
+++ python_accounting/database/event_listeners.py
@@ -11,14 +11,12 @@ that are relevant to accounting.
 
 """
 from datetime import datetime
-
-from sqlalchemy.orm.session import Session
 from sqlalchemy import event, orm, and_, update
+from sqlalchemy.orm.session import Session
 
 from python_accounting.models import Entity, Recyclable, Transaction, Account, Ledger
 from python_accounting.mixins import IsolatingMixin
 from python_accounting.exceptions import MissingEntityError
-
 
 def _filter_options(execute_state, option) -> bool:
     return (
@@ -28,97 +26,112 @@ def _filter_options(execute_state, option) -> bool:
         and not execute_state.session.info.get(option, False)
     )
 
-
-# pylint: disable=too-few-public-methods
-class EventListenersMixin:
-    """
-    Event Listeners class.
-    """
-
-    @event.listens_for(Session, "do_orm_execute")
-    def _add_filtering_criteria(  # pylint: disable=no-self-argument
-        execute_state,
-    ) -> None:
-        # Recycling filter
-        if _filter_options(execute_state, "include_deleted"):
-            execute_state.statement = execute_state.statement.options(
-                orm.with_loader_criteria(
-                    Recyclable,
-                    lambda cls: and_(
-                        cls.deleted_at == None,  # pylint: disable=singleton-comparison
-                        cls.destroyed_at  # pylint: disable=singleton-comparison
-                        == None,
-                    ),
-                    execute_state,
-                    include_aliases=True,
-                )
+# Create standalone functions instead of class methods
+def _add_filtering_criteria(execute_state) -> None:
+    # Recycling filter
+    if _filter_options(execute_state, "include_deleted"):
+        execute_state.statement = execute_state.statement.options(
+            orm.with_loader_criteria(
+                Recyclable,
+                lambda cls: and_(
+                    cls.deleted_at == None,  # pylint: disable=singleton-comparison
+                    cls.destroyed_at  # pylint: disable=singleton-comparison
+                    == None,
+                ),
+                execute_state,
+                include_aliases=True,
             )
+        )
 
-        # Entity filter
-        if (
-            _filter_options(execute_state, "ignore_isolation")
-            and execute_state.statement.column_descriptions[0]["type"] is not Entity
-        ):
-            session_entity_id = execute_state.session.entity.id
-            execute_state.statement = execute_state.statement.options(
-                orm.with_loader_criteria(
-                    IsolatingMixin,
-                    lambda cls: cls.entity_id == session_entity_id,
-                    include_aliases=True,
-                )
-            )
-
-    @event.listens_for(Session, "transient_to_pending")
-    def _set_session_entity(self, object_) -> None:
-        if not hasattr(self, "entity") or self.entity is None:
-            if isinstance(object_, Entity):
-                self.entity = object_
-            elif object_.entity_id is None:
-                raise MissingEntityError
-            else:
-                self.entity = self.get(Entity, object_.entity_id)
-
-        if (
-            self.entity.reporting_period is None
-            or self.entity.reporting_period.calendar_year != datetime.today().year
-        ):
-            self._set_reporting_period()
-
-    @event.listens_for(Session, "transient_to_pending")
-    def _set_object_index(self, object_) -> None:
-        if (isinstance(object_, (Account, Transaction))) and object_.id is None:
-            object_.session_index = (
-                len(
-                    [
-                        t
-                        for t in self.new
-                        if isinstance(t, Transaction)
-                        and t.transaction_type == object_.transaction_type
-                    ]
-                )
-                if isinstance(object_, Transaction)
-                else len(
-                    [
-                        a
-                        for a in self.new
-                        if isinstance(a, Account)
-                        and a.account_type == object_.account_type
-                    ]
-                )
-            )
-
-    @event.listens_for(Ledger, "after_insert")
-    def _set_ledger_hash(  # pylint: disable=no-self-argument
-        mapper, connection, target
+    # Entity filter
+    if (
+        _filter_options(execute_state, "ignore_isolation")
+        and execute_state.statement.column_descriptions[0]["type"] is not Entity
     ):
+        session_entity_id = execute_state.session.entity.id
+        execute_state.statement = execute_state.statement.options(
+            orm.with_loader_criteria(
+                IsolatingMixin,
+                lambda cls: cls.entity_id == session_entity_id,
+                include_aliases=True,
+            )
+        )
+
+def _set_session_entity(session, object_) -> None:
+    if not hasattr(session, "entity") or session.entity is None:
+        if isinstance(object_, Entity):
+            session.entity = object_
+        elif object_.entity_id is None:
+            raise MissingEntityError
+        else:
+            session.entity = session.get(Entity, object_.entity_id)
+
+    if (
+        session.entity.reporting_period is None
+        or session.entity.reporting_period.calendar_year != datetime.today().year
+    ):
+        session._set_reporting_period()
+
+def _set_object_index(session, object_) -> None:
+    if (isinstance(object_, (Account, Transaction))) and object_.id is None:
+        object_.session_index = (
+            len(
+                [
+                    t
+                    for t in session.new
+                    if isinstance(t, Transaction)
+                    and t.transaction_type == object_.transaction_type
+                ]
+            )
+            if isinstance(object_, Transaction)
+            else len(
+                [
+                    a
+                    for a in session.new
+                    if isinstance(a, Account)
+                    and a.account_type == object_.account_type
+                ]
+            )
+        )
+
+def _validate_model(session, _, __) -> None:
+    for model in list(session.new) + list(session.dirty):
+        if hasattr(model, "validate"):
+            model.validate(session)
+
+# The Ledger event handler stays at the model level but checks for accounting sessions
+@event.listens_for(Ledger, "after_insert")
+def _set_ledger_hash(mapper, connection, target):
+    # Only proceed if session has our entity attribute (our custom session type)
+    if hasattr(target, "_sa_instance_state") and \
+       hasattr(target._sa_instance_state.session, "entity"):
         connection.execute(
             update(Ledger)
             .where(Ledger.id == target.id)
             .values(hash=target.get_hash(connection))
         )
 
-    @event.listens_for(Session, "before_flush")
-    def _validate_model(self, _, __) -> None:
-        for model in list(self.new) + list(self.dirty):
-            if hasattr(model, "validate"):
-                model.validate(self)
+def register_accounting_events(session_factory):
+    """
+    Register accounting-specific event listeners to a specific session factory
+    rather than globally to the Session class.
+    
+    Args:
+        session_factory: A sessionmaker instance
+    
+    Returns:
+        The modified sessionmaker instance
+    """
+    event.listen(session_factory, "do_orm_execute", _add_filtering_criteria)
+    event.listen(session_factory, "transient_to_pending", _set_session_entity)
+    event.listen(session_factory, "transient_to_pending", _set_object_index)
+    event.listen(session_factory, "before_flush", _validate_model)
+    return session_factory
+
+# Keep the class for backward compatibility, but it no longer registers global listeners
+class EventListenersMixin:
+    """
+    Event Listeners class - now just a placeholder for backward compatibility.
+    Global listeners are not registered anymore.
+    """
+    pass
--- python_accounting/database/session.py
+++ python_accounting/database/session.py
@@ -9,13 +9,13 @@
 Provides accounting specific overrides for some sqlalchemy session methods.
 
 """
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
 from python_accounting.models import Entity
 from python_accounting.database.session_overrides import SessionOverridesMixin
 from python_accounting.database.accounting_functions import AccountingFunctionsMixin
-from python_accounting.database.event_listeners import EventListenersMixin
-
+from python_accounting.database.event_listeners import EventListenersMixin, register_accounting_events
 
 class AccountingSession(
     SessionOverridesMixin, EventListenersMixin, AccountingFunctionsMixin, Session
@@ -27,12 +27,32 @@ class AccountingSession(
         entity (Entity): The Entity currently associated with the session. All database
             queries will be scoped to this entity.
     """
-
     entity: Entity
 
-    def __init__(self, bind=None, info=None) -> None:
-        super().__init__(bind=bind, info=info)
+    def __init__(self, bind=None, info=None, **kwargs) -> None:
+        super().__init__(bind=bind, info=info, **kwargs)
 
+def get_session_factory(engine):
+    """
+    Create a session factory with accounting-specific event listeners.
+    
+    Args:
+        engine: The database engine to create sessions for.
+        
+    Returns:
+        A sessionmaker that will create AccountingSession instances with proper event listeners.
+    """
+    factory = sessionmaker(
+        bind=engine,
+        class_=AccountingSession,
+        info={
+            "include_deleted": engine.get_execution_options().get("include_deleted", False),
+            "ignore_isolation": engine.get_execution_options().get("ignore_isolation", False),
+        }
+    )
+    
+    # Register event listeners on this specific factory
+    return register_accounting_events(factory)
 
 def get_session(engine) -> Session:
     """
@@ -43,17 +63,6 @@ def get_session(engine) -> Session:
 
     Returns:
         AccountingSession.
-
     """
-
-    return AccountingSession(
-        bind=engine,
-        info={
-            "include_deleted": engine.get_execution_options().get(
-                "include_deleted", False
-            ),
-            "ignore_isolation": engine.get_execution_options().get(
-                "ignore_isolation", False
-            ),
-        },
-    )
+    session_factory = get_session_factory(engine)
+    return session_factory()
